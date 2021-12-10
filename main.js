2ba4fe87577d1285343b8157ea2a9b07361094bc
SNAILTHELAZY <player3755@gmail.com>
2021-12-10 16:04:12 +0800

    3 files modified

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import * as Chart from './chart.js';
 import * as Plotly from './plotly.js';
+import ApexCharts from 'apexcharts';
 
 /**
  * Comparison between Chart.js and Plotly
@@ -10,9 +11,12 @@ import * as Plotly from './plotly.js';
  * 3. Does not have Toolkits
  *
  * Plotly:
- * 1. Not Responsive
+ * 1. Not Responsive (requires reloads upon the screensize changes)
  * 2. Does not have Animation
  * 3. Has Toolkits
+ *
+ * ApexCharts:
+ *
  */
 
 const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
@@ -29,3 +33,20 @@ const plotlyLineChart = Plotly.createLineChart(
   values,
   document.getElementById('plotly')
 );
+
+var options = {
+  chart: {
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'sales',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+  ],
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+};
+var chart = new ApexCharts(document.querySelector('#apex'), options);
+chart.render();
